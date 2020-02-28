0ff8a9b711b9c71b87d33aa88f65833c0fcf0348
thebangtanepiphany <thebangtanepiphany@gmail.com>
2020-02-28 15:47:29 -0800

    router, styling, reorg files

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import React from "react";
-import em from "./img/emily.png"
-import rk from "./img/rocky.png"
-import Nav from "./components/Nav"
-import Title from "./components/Title"
-import "./App.css";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Episodes from "./pages/Episodes"
+import TwitterIcon from "./img/twitter"
+import logo from "./img/bangtan-icon-sm.png"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Nav />
-      </header>
-      <section className="header">
-        <Title />
-      </section>
-      <section className="about-us" id="about-us">
-        <div className="spacer"></div>
-        <h2>About Us</h2>
-        <div className="card-deck">
-          <div className="card bg-light">
-            <img className="card-img-top about-img img-fluid image-responsive" alt="Emily" src={em} />
-            <div className="card-body">
-              <p className="card-text">Emily is a programmer, writer, and proud ARMY circa March 9, 2019 <span role="img" aria-label="winking face">&#128521;</span>. She loves to talk about culture, art, music... really just about anything. She may or may not be an immortal vampire.</p>
-            </div>
-          </div>
-          <div className="card bg-light">
-            <img className="card-img-top about-img img-fluid image-responsive" alt="Rocky" src={rk} />
-            <div className="card-body">
-              <p className="card-text">Rocky is a programmer, gamer and baby ARMY. He loves music and finding those deeper meanings.</p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="podcast-episodes" id="episodes">
-        <div className="spacer"></div>
-        <h2>Episodes</h2>
-        <div className="player">
-          <div id="buzzsprout-large-player-814801"></div>
-        </div>
-      </section>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav className="nav fixed-top">
+            <a className="nav-icon" href="/">
+              <img className="" alt="The Bangtan Epiphany Podcast" src={logo} />
+            </a>
+            <ul className="nav-links">
+              <li>
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/about">About us</Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/episodes">Episodes</Link>
+              </li>
+            </ul>
+            <a className="nav-icon social" href="https://www.twitter.com/btsepiphanypod" target="_blank" rel="noopener noreferrer">
+              <TwitterIcon/>
+            </a>
+          </nav>
+        </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/episodes" component={Episodes} />
+      </Switch>
+      </div>
+    </Router>
   )
 }
 
