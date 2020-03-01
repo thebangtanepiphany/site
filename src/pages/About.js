4a497e522c85a98837c91c9f8a3c8ee4ccc85072
thebangtanepiphany <57727292+thebangtanepiphany@users.noreply.github.com>
2020-02-29 22:38:13 -0800

    UI Update
    CSS updates (mobile)
    about page copy update

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,13 +12,13 @@ export default class About extends React.Component {
           <div className="card">
             <img className="card-img" alt="Emily" src={em} />
             <div className="card-body">
-              <p className="card-text">Emily is a unpublished writer, parttime painter and fulltime ARMY circa March 9, 2019 <span role="img" aria-label="winking face">&#128521;</span>. She loves to talk about culture, art, music... really just about anything. She may or may not be an immortal vampire.</p>
+              <p className="card-text">Emily is an unpublished writer, part-time painter and full-time ARMY circa March 9, 2019 <span role="img" aria-label="winking face">&#128521;</span>. She loves to talk about culture, art, music... really just about anything. She may or may not be an immortal vampire.</p>
             </div>
           </div>
           <div className="card">
             <img className="card-img" alt="Rocky" src={rk} />
             <div className="card-body">
-              <p className="card-text">Rocky got into BTS because he loves good music and honest lyrics. He's into hip hop, Final Fantasy, Killing Eve and bad action movies.</p>
+              <p className="card-text">Rocky got into BTS because he loves good music and honest lyrics. He's into hip-hop, Final Fantasy, Killing Eve and bad action movies.</p>
             </div>
           </div>
         </div>
