a32d5120848a86f98abba646048d8aefd79d5585
thebangtanepiphany <thebangtanepiphany@gmail.com>
2020-03-03 09:54:17 -0800

    adding episode card and data, dynamic episode building

---
+++ src/components/EpisodeCard.js
@@ -0,0 +1,40 @@
+import React from "react"
+import PropTypes from "prop-types"
+import "./EpisodeCard.css"
+
+export default class EpisodeCard extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="top">
+          <div className="number">Ep. {this.props.number}</div>
+          <h2 className="title">{this.props.title}</h2>
+          <div className="date">{this.props.date}</div>
+        </div>
+        <div className="middle">
+          <p className="description">{this.props.description}</p>
+          {
+            this.props.links.map((link) => {
+              return (
+                <div className="link">
+                  <p>{link.text}</p>
+                  <a href={link.url}>{link.title}</a>
+                </div>
+              )
+            })
+          }
+        </div>
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
+// Props
+EpisodeCard.propTypes = {
+  date: PropTypes.number, //six digit number in format YYMMDD
+  number: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  links: PropTypes.arrayOf(PropTypes.object),
+}
--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -1,20 +1,50 @@
 import React from "react"
+import EpisodeCard from "../components/EpisodeCard"
+import episodeData from "../data/Episodes.json"
 import "./Episodes.css"
 
 export default class Episodes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { episodes: [] };
+    this.createEmbedPlayer = this.createEmbedPlayer.bind(this);
+  }
+
   componentDidMount() {
+    let episodes = episodeData.map((data) => {
+      return (
+        <EpisodeCard
+          date={data.date}
+          number={data.number}
+          title={data.title}
+          description={data.description}
+          links={data.links}>
+          {this.createEmbedPlayer(data.embedId, data.embedNum)}
+        </EpisodeCard>
+      )
+    })
+    this.setState({episodes: episodes})
+  }
+
+  createEmbedPlayer(embedId, embedNum) {
+    const id = `buzzsprout-player-${embedNum}`
+    const scriptSrc = `https://www.buzzsprout.com/814801/${embedId}.js?container_id=buzzsprout-player-${embedNum}&player=small`
     const script = document.createElement("script")
-    script.src = "https://www.buzzsprout.com/814801.js?container_id=buzzsprout-player&player=small"
+    script.src = scriptSrc
+    script.charset = "utf-8"
     script.async = true
     document.body.appendChild(script)
+    return (
+      <div id={id}></div>
+    )
   }
 
   render() {
     return (
       <div>
         <h2>Episodes</h2>
-        <div className="player">
-          <div id="buzzsprout-player"></div>
+        <div className="episodes">
+          {this.state.episodes}
         </div>
       </div>
     )
